2449b84e8e29858f5d76271cb3fb25ee21bd787d
Florian Meinel <florian.meinel@student.hpi.uni-potsdam.de>
2016-12-12 16:40:08 +0100

    liberty parser: split blocks by indent, pass cell statements to cell parser stub

---
+++ src/main/java/de/uni_potsdam/hpi/asg/drivestrength/cells/libertyparser/LibertyCellParser.java
@@ -0,0 +1,24 @@
+package de.uni_potsdam.hpi.asg.drivestrength.cells.libertyparser;
+
+import java.util.List;
+
+import de.uni_potsdam.hpi.asg.drivestrength.cells.Cell;
+
+public class LibertyCellParser {
+
+    private List<String>statements;
+    
+    public LibertyCellParser(List<String> statements) {
+        this.statements = statements;
+    }
+    
+    public Cell run() {
+        Cell cell = new Cell();
+        
+        
+        
+        
+        return cell;
+    }
+    
+}
--- src/main/java/de/uni_potsdam/hpi/asg/drivestrength/cells/libertyparser/LibertyParser.java
+++ src/main/java/de/uni_potsdam/hpi/asg/drivestrength/cells/libertyparser/LibertyParser.java
@@ -3,11 +3,17 @@ package de.uni_potsdam.hpi.asg.drivestrength.cells.libertyparser;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.uni_potsdam.hpi.asg.common.iohelper.FileHelper;
 import de.uni_potsdam.hpi.asg.drivestrength.cells.Cell;
 
 public class LibertyParser {
+
+    private static final Pattern indentPattern = Pattern.compile("(\\s*)(.*)");
+    private static final Pattern startCellPattern = Pattern.compile("(\\s*)cell\\s*\\((.*)\\)\\s*");
+    
     private List<String> statements;
 
     public LibertyParser(File libertyFile) {
@@ -16,9 +22,30 @@ public class LibertyParser {
     
     public List<Cell> run() {
         System.out.println("Parsing Liberty File\n");
+        
         List<Cell> cells = new ArrayList<>();
+
+        List<String> currentCellStatements = new ArrayList<String>();
+        
+        boolean isReadingCellBlock = false;
+        int cellBlockIndent = -1;
         for (String statement : statements) {
-            System.out.println("Statement: " + statement);
+            Matcher m = indentPattern.matcher(statement);
+            if (isReadingCellBlock && m.matches()) {
+                if (m.group(1).length() == cellBlockIndent) {
+                    cells.add(new LibertyCellParser(currentCellStatements).run());
+                    isReadingCellBlock = false;
+                } else {
+                    currentCellStatements.add(statement);
+                }
+            }
+            m = startCellPattern.matcher(statement);
+            if (m.matches()) {
+                isReadingCellBlock = true;
+                cellBlockIndent = m.group(1).length();
+                currentCellStatements = new ArrayList<String>();
+                currentCellStatements.add(statement);
+            }
         }
         return cells;
     }
@@ -34,9 +61,24 @@ public class LibertyParser {
     private List<String> mergeMultilineStatements(List<String> lines) {
         List<String> statements = new ArrayList<String>();
         
-        //String statement = "";
+        String statement = "";
+        boolean trimNext = false;
         for (String line: lines) {
-            statements.add(line);
+            line = line.replaceAll("\\{|\\}", "");
+            if (trimNext) {
+                line = line.trim();
+                trimNext = false;
+            }
+            if (line.trim().length() == 0)
+                continue;
+            if (line.charAt(line.length() - 1) == '\\') {
+                statement += line.substring(0, line.length() - 1);
+                trimNext = true;
+            } else {
+                statement += line;
+                statements.add(statement);
+                statement = "";
+            }
         }
         
         return statements;
