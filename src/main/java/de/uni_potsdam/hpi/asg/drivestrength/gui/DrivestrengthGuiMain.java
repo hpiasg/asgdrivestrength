562cc59a8d697d860a80fb9c58452deac11348d0
Florian Meinel <florian.meinel@student.hpi.uni-potsdam.de>
2017-03-31 14:43:59 +0200

    gui stub

---
+++ src/main/java/de/uni_potsdam/hpi/asg/drivestrength/gui/DrivestrengthGuiMain.java
@@ -0,0 +1,7 @@
+package de.uni_potsdam.hpi.asg.drivestrength.gui;
+
+public class DrivestrengthGuiMain {
+    public static void main(String[] args) {
+        new DrivestrengthMainWindow();
+    }
+}
