3b7d51b70ccacff2c720b37434c9de01c87d6ba7
Florian Meinel <florian.meinel@student.hpi.uni-potsdam.de>
2017-05-04 13:44:31 +0200

    netlist flattener: dont rename modules that are unique in the netlist

--- src/main/java/de/uni_potsdam/hpi/asg/drivestrength/netlist/cleaning/NetlistFlattener.java
+++ src/main/java/de/uni_potsdam/hpi/asg/drivestrength/netlist/cleaning/NetlistFlattener.java
@@ -12,48 +12,63 @@ import de.uni_potsdam.hpi.asg.drivestrength.netlist.elements.ModuleInstance;
 
 public class NetlistFlattener {
     protected static final Logger logger = LogManager.getLogger();
-    
+
     private Netlist netlist;
     private List<Module> flattenedModules;
 
     public NetlistFlattener(Netlist netlist) {
         this.netlist = netlist;
     }
-    
+
     public void run() {
         this.flattenedModules = new ArrayList<Module>();
-        
+
         this.flattenFromModule(netlist.getRootModule());
         this.flattenedModules.add(netlist.getRootModule());
         netlist.setModules(this.flattenedModules);
     }
-    
+
     private void addModuleUnique(Module module) {
         if (!this.flattenedModules.contains(module)) {
             this.flattenedModules.add(module);
         }
     }
-    
+
     private void flattenFromModule(Module module) {
         for (ModuleInstance instance : module.getModuleInstances()) {
             Module definition = instance.getDefinition();
-            if (definition.hasAssignStatementsOnly()) {
+            if (definition.hasAssignStatementsOnly() || this.isUniqueInNetlist(definition)) {
                 addModuleUnique(definition);
                 continue;
             }
-            Module flattenedModule = this.createFlattenedCopy(definition, 
+            Module flattenedModule = this.createFlattenedCopy(definition,
                                                  instance.getName(), module.getName());
             instance.setDefinition(flattenedModule);
             flattenFromModule(flattenedModule);
             // add *after* recursive call to preserve hierarchical ordering
-            flattenedModules.add(flattenedModule); 
+            flattenedModules.add(flattenedModule);
         }
     }
-    
+
+    private boolean isUniqueInNetlist(Module testedModule) {
+        boolean foundOnce = false;
+        for (Module eachModule : this.netlist.getModules()) {
+            for (ModuleInstance eachModuleInstance : eachModule.getModuleInstances()) {
+                if (eachModuleInstance.getDefinition().getName() == testedModule.getName()) {
+                    if (foundOnce) {
+                        return false;
+                    }
+                    foundOnce = true;
+                }
+            }
+        }
+        return true;
+    }
+
     private Module createFlattenedCopy(Module originalModule, String instanceName, String parentName) {
         Module module = new Module(originalModule);
         module.setName(parentName + "__" + module.getName() + "___" + instanceName);
         return module;
     }
-    
+
 }
